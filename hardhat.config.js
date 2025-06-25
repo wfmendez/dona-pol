b7ce7002284353f4077b4212f74ce9f4cb162f57
wfmendez <wmendezgarcia@byupathway.edu>
2025-06-24 20:58:34 -0400

    build: Configure Amoy testnet deployment environment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    // Config Polygon Amoy Testnet Network
+    amoy: {
+      url: process.env.ALCHEMY_AMOY_RPC_URL || "https://rpc-amoy.polygon.technology/", // Amoy RPC URL
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 80002, // Amoy Chain ID
+    },
+  },
 };
